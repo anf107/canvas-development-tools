a7b0e311d3b6cce4443b8049166bff2aa27b6e77
anf107 <anf107@psu.edu>
2016-12-01 16:20:05 -0500

    Update build.sh

--- build.sh
+++ build.sh
@@ -16,9 +16,6 @@ curl -O https://raw.githubusercontent.com/FreedomBen/canvas-development-tools/ma
 chmod +x CODES.sh
 ./CODES.sh --full
 
-# NOW, run CODES.sh
-./CODES.sh --full
-
 # after the setup completes, cd to the canvas root directory, and point your browser to (server):3000
 cd ~/canvas-lms
 bundle exec rails server
